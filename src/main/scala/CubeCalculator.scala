63bbcd1514bf4f1bfabf5c6445cd120e9394ac49
Bouchra Bennani <gaufre-b@michelblancard.fr>
2020-09-16 20:24:45 +0200

    Add covered and uncovered code to test coverage report

--- src/main/scala/CubeCalculator.scala
+++ src/main/scala/CubeCalculator.scala
@@ -1,5 +1,11 @@
 object CubeCalculator {
   def cube(x: Int) = {
+    println("Hello this is covered code")
     x * x * x
+  }
+
+  def square(x: Int)= {
+    println("Hello this is code not covered")
+    x * x
   }
 }
