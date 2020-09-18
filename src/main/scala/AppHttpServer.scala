1999d7219015b084fd3fce998f54d3dd2ada827c
Carl Christensen <gaufre-c@michelblancard.fr>
2020-09-18 17:27:44 +0200

    Use Future.never instead of readLine to wait

--- src/main/scala/AppHttpServer.scala
+++ src/main/scala/AppHttpServer.scala
@@ -2,6 +2,8 @@
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration
 
 import scala.io.StdIn
 
@@ -10,15 +12,22 @@ object AppHttpServer {
     def main(args: Array[String]): Unit = {
 
         implicit val system = ActorSystem(guardianBehavior=Behaviors.empty, name="my-system")
-        // needed for the future flatMap/onComplete in the end
         implicit val executionContext = system.executionContext
 
         val bindingFuture = Http().newServerAt("localhost", 8080).bind(Routes.routes)
 
-        println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-        StdIn.readLine() // let it run until user presses return
-        bindingFuture
-        .flatMap(_.unbind()) // trigger unbinding from the port
-        .onComplete(_ => system.terminate()) // and shutdown when done
+        val serverStartedFuture = bindingFuture.map(binding => {
+            val address = binding.localAddress
+            println(s"Server online at http://${address.getHostString}:${address.getPort}/")
+        })
+
+        val waitOnFuture = serverStartedFuture.flatMap(unit => Future.never)
+        
+        sys.addShutdownHook { 
+            // cleanup logic
+            // nothing to do for now
+        }
+
+        Await.ready(waitOnFuture, Duration.Inf)
     }
 }
