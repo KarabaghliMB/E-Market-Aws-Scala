7b18a2969dc5952c655304953b1a9dc3012e9896
Carl Christensen <gaufre-c@michelblancard.fr>
2020-09-23 18:25:06 +0200

    Add a script to create the table Users

---
+++ src/main/scala/CreateTables.scala
@@ -0,0 +1,32 @@
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration
+import com.typesafe.scalalogging.LazyLogging
+
+
+object CreateTables extends LazyLogging {
+
+    def main(args: Array[String]): Unit = {
+        implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+        val db = MyDatabase.db
+        var exitCode = 0
+
+        val creationFuture: Future[Unit] = Users.createTable
+
+        val successCase: Future[Unit] = creationFuture.map(_ => logger.info("Done creating table Users"))
+        val failureCase: Future[Unit] = creationFuture.failed.map(exc => {
+            logger.error("Could not create table Users: " + exc)
+            exitCode = 1
+        })
+
+        val combinedFuture = for {
+            _ <- successCase
+            _ <- failureCase
+        } yield akka.Done
+
+        Await.ready(combinedFuture, Duration.Inf)
+
+        db.close
+
+        System.exit(exitCode)
+    }
+}
