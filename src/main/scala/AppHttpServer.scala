a67de3493712931e54102d61467d18db543ebf4f
Carl Christensen <gaufre-c@michelblancard.fr>
2020-09-25 23:39:02 +0200

    Configure the test database

--- src/main/scala/AppHttpServer.scala
+++ src/main/scala/AppHttpServer.scala
@@ -13,6 +13,7 @@ object AppHttpServer extends LazyLogging {
         implicit val actorsSystem = ActorSystem(guardianBehavior=Behaviors.empty, name="my-system")
         implicit val actorsExecutionContext = actorsSystem.executionContext
 
+        MyDatabase.initialize("mydb")
         val db = MyDatabase.db
 
         val bindingFuture = Http().newServerAt("0.0.0.0", 8080).bind(Routes.routes)
--- src/main/scala/MyDatabase.scala
+++ src/main/scala/MyDatabase.scala
@@ -8,5 +8,12 @@ import slick.jdbc.PostgresProfile.api._
 
 object MyDatabase {
     implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
-    val db = Database.forConfig("mydb")
+    var db: Database = null
+    
+    // Run this method at the entrypoint. This is a kind of manual dependency injection to make it possible to test against a test database.
+    // After, you can get the member `db` and use it directy.
+    // At the end, call `db.close`.
+    def initialize(configName: String): Unit = {
+        db = Database.forConfig(configName)
+    }
 }
